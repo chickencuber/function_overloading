f855103a84fc0a239c26f1f48c345fbcab6cd9d0
chickencuber <n1batman728@gmail.com>
2024-11-08 17:51:22 -0600

    added public function

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,33 @@ macro_rules! def {
              )*
         } 
     };
+    (pub $name:ident; $(fn ($($arg:ident : $type:ty),* $(,)?) $(-> $ret:ty)? $body:block);+ $(;)?) => {
+        $crate::paste! {
+            $crate::helper! {
+                ($d:tt) => {
+                    #[macro_export]
+                    macro_rules! $name {
+                        ($d($d args:expr),*) => {
+                            ($d($d args),*).[<fn_ $name>]()
+                        }
+                    }
+                }
+            }
+            pub trait [<Fn $name:camel>] {
+                type [<Fn $name:camel Return>];
+                fn [<fn_ $name>](&self) -> Self::[<Fn $name:camel Return>];
+            }
+            $(  
+                 impl [<Fn $name:camel>] for ($($type),*) {
+                    type [<Fn $name:camel Return>] = $crate::helper_type_default!($($ret)?, ());
+                    fn [<fn_ $name>](&self) -> Self::[<Fn $name:camel Return>] {
+                        let ($($arg),*) = self;
+                        $body
+                    }
+                }
+             )*
+        } 
+    };
 }
 
 #[macro_export]
@@ -47,4 +74,24 @@ macro_rules! helper_type_default {
         $type
     };
 }
+
+def! {
+    pub foo;
+    fn (a: u32, b: u32) -> u32 {
+        return a + b;
+    };
+    fn (a: &str, b: &str) -> usize {
+        return a.len() + b.len();
+    };
+    fn (a: String) {
+        println!("{}", a);
+    };
+    fn (a: &str) {
+        println!("{}", a);
+    };
+    fn(a: f32, b: f32) -> f32 {
+        return a * b;
+    };
+}
+
 
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,9 @@
-use function_overloading::def;
-
-def! {
-    foo;
-    fn (a: u32, b: u32) -> u32 {
-        return a + b;
-    };
-    fn (a: &str, b: &str) -> usize {
-        return a.len() + b.len();
-    };
-    fn (a: String) {
-        println!("{}", a);
-    };
-}
+use function_overloading::{foo, FnFoo};
 
 fn main() {
     println!("{}", foo!("1", "2"));
     println!("{}", foo!(1, 2));
+    println!("{}", foo!(1.0, 2.0));
     foo!(String::from("hello"));
+    foo!("world");
 }
